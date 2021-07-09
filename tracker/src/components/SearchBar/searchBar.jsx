8be0b63c25234db53d303973a7100a8cbcb59197
Hinrik <hinrik.helga@gmail.com>
2021-07-09 13:11:21 +0200

    functional search bar with button implemented
    Some stylings and background image added

--- tracker/src/components/SearchBar/searchBar.jsx
+++ tracker/src/components/SearchBar/searchBar.jsx
@@ -1,8 +1,34 @@
 import React, { useState } from 'react';
+import styles from './searchBar.module.css'
+import { ReactComponent as SvgArrow } from '../../images/icon-arrow.svg';
+
 
 export const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState(() => initSearchTerm());
+
+    function initSearchTerm() {
+        return '';
+    }
+
+    function handleChange(event)  {
+        setSearchTerm(event.target.value);
+    }
+
+    function handleClick() {
+        console.log(searchTerm);
+    }
+
     return (
-        <input> 
-        </input>
+        <>
+            <input
+                className={styles.searchBar}
+                placeholder={'Search for any IP address or domain'}
+                value={searchTerm}
+                onChange={handleChange}
+            /> 
+            <button className={styles.btn} onClick={handleClick}>
+                <SvgArrow></SvgArrow>
+            </button>
+        </>
     );
 }
