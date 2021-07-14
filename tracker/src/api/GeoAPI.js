938320f24c6596bdce56102a6a28da7d256765fc
Hinrik <hinrik.helga@gmail.com>
2021-07-14 17:48:28 +0200

    padding fix

--- tracker/src/api/GeoAPI.js
+++ tracker/src/api/GeoAPI.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 // personal API key
 const key = 'at_41J81iwawTcaSlH77cnJ8TkNSkHc3';
+const newKey = 'at_A0lvgKf06y3byvbLj9CPOUtn20IWI';
 
 export const getIPAddress = (address) => {
-    let endpoint = `https://geo.ipify.org/api/v1?apiKey=${key}`;
+    let endpoint = `https://geo.ipify.org/api/v1?apiKey=${newKey}`;
     if (address !== '' && address !== undefined) {
-        endpoint = `https://geo.ipify.org/api/v1?apiKey=${key}&ipAddress=${address}`;
+        endpoint = `https://geo.ipify.org/api/v1?apiKey=${newKey}&ipAddress=${address}`;
     }
     return axios.get(endpoint);
 }
